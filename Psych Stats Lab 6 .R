647e5c826fd8d5b47d6cac7cbb7ed65a4d95dcc1
dyeboah4 <157760266+dyeboah4@users.noreply.github.com>
2024-03-30 18:24:40 -0400

    Update Psych Stats Lab 6 .R

--- Psych Stats Lab 6 .R
+++ Psych Stats Lab 6 .R
@@ -39,3 +39,13 @@ however it is good to know for example, if your a substance abuse counselor, tha
 For Life satifaction there were a statitcally signifant amount of responses for satified and also a statitically signifant amount of responses for i dont know and refused responses. Before running the code I assumed that 
 more satified people will be using marijuana more than the not satified people due to the euphoric effects of the drug. However the data shows that the responeses for Marijuana users and their life satification most users
 did not answer or did not know which shows that there are most likelty other things interacting with satification beside Marijuana use that have an effect on people's answers. 
+
+
+The article that I read this week for HW was "Smartphones, social media use and youth mental health" by Elia Abi-Jaoude MSc MD, Karline Treurnicht Naylor MPH MD, Antonio Pignatiello MD. This was a review paper, that looked at a
+multitude of studies on social media usage and teens mental health. The researchers looked at various cross-sectional longitudinal, and emprirical studies 
+that looked at social media and smartphone use among North American youth. Their research finds that North American youth spend a lot of time on social media and that there has been a rise of social media usage
+and cell phone usage over the last couple of years. Their findings show that social media use can lead to increase in mental distress, self injurous behaviors, and negative self view. 
+Their reasoning for this came from how social media will "normalize" self harm and even suicide. Another factor that the reseachers acknowledge is that youth are using social media and multiple apps during the night
+and are sleeping less than normal, having negative effects on cognitive control and academic performance. Their statstical data the paper is using comes from various cross-sectional longitudinal, and emprirical studies that have looked
+at social media/cell phone usage and North American Teens. I hope to use the data here for my project and create charts and grapghs explaining how social media use are effecting North American youth negatively. I want to specifically look at
+the relationship between academics, life statifaction, mental health, physical health, sleep patterns and how they compare wit social media usage among youth. 
